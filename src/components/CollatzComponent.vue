0703d7e84f7706b087b626cef01a57109804aea3
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-27 11:48:10 +0100

    collatz con filters

--- src/components/CollatzComponent.vue
+++ src/components/CollatzComponent.vue
@@ -3,8 +3,11 @@
 	<label>Introduzca Numero:</label>
 	<input type="number" v-model="numero" />
 	<button v-on:click="getCollatz">Collatz</button>
+	<div v-html="evaluarNumero"></div>
 	<ul>
-		<li v-for="numero in listacollatz" :key="numero">{{ numero }}</li>
+		<li v-for="numero in listacollatz" :key="numero">
+			{{ numero }}
+		</li>
 	</ul>
 </template>
 <script>
@@ -15,6 +18,12 @@
 				numero: 0,
 				listacollatz: [],
 			};
+		},
+		computed: {
+			evaluarNumero() {
+				let dibujo = this.$filters.parImpar(this.listacollatz[0]);
+				return dibujo;
+			},
 		},
 		methods: {
 			getCollatz() {
--- src/main.js
+++ src/main.js
@@ -4,4 +4,16 @@ import router from './Router.js'
 
 let app = createApp(App)
 
+app.config.globalProperties.$filters = {
+    parImpar(numero) {
+        let dibujo = "";
+        if (numero % 2 == 0) {
+            dibujo = "<span style='color:green'>Par</span>"
+        } else {
+            dibujo = "<span style='color:red'>Impar</span>"
+        }
+        return dibujo;
+    }
+}
+
 app.use(router).mount('#app')
